c94644d752101cd83578e71dc800c3dcc6abb72c
Joop Laan <joop.laan@gmail.com>
2021-12-12 09:16:00 +0100

    Update search form focus and active style

--- searchform.php
+++ searchform.php
@@ -13,6 +13,6 @@
 
 		<label class="govuk-label search__label" for="search" aria-hidden="true"><?php esc_html_e( 'Search this website', 'saltato' ); ?></label>
 		<input type="text" id="search" name="q" class="govuk-input govuk-!-margin-bottom-0 search__input" aria-controls="search-results" placeholder="<?php esc_html_e( 'Search', 'saltato' ); ?>" value="<?php echo esc_url( get_search_query() ); ?>">
-		<button type="submit" class="search__button"><?php esc_html_e( 'Search', 'saltato' ); ?></button>
+		<button type="submit" class="search__button" data-module="govuk-button"><?php esc_html_e( 'Search', 'saltato' ); ?></button>
 	</form>
 </div>
