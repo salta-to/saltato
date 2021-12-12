2acdfcb108de62c3216f9984c1adc2eeb8d3977a
Joop Laan <joop.laan@gmail.com>
2021-12-12 10:27:27 +0100

    Update function number

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 
 if ( ! defined( 'SALTATO_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( 'SALTATO_VERSION', '1.0.0' );
+	define( 'SALTATO_VERSION', '1.1.2' );
 }
 
 if ( ! function_exists( 'saltato_setup' ) ) :
