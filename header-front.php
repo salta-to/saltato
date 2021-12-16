e91ee8460526b4d95ccd9895a50bea21db2c50a5
Joop Laan <joop.laan@gmail.com>
2021-12-16 09:38:19 +0100

    Update header

--- header-front.php
+++ header-front.php
@@ -24,47 +24,69 @@
 <a href="#main-content" class="govuk-skip-link"><?php esc_html_e( 'Skip to main content', 'saltato' ); ?></a>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-
-	<header class="govuk-header " role="banner" data-module="govuk-header">
-		<div class="govuk-header__container govuk-width-container">
-			<div class="govuk-grid-row">
-			<div class="govuk-grid-column-two-thirds-from-desktop">
-				<a class="link-header-home" href="<?php echo esc_url( home_url() ); ?>">
-				<?php
-				$custom_logo_id = get_theme_mod( 'custom_logo' );
-				if ( $custom_logo_id ) :
-					$image = wp_get_attachment_image_src( $custom_logo_id, 'full' );
-					?>
+	<header class="govuk-header app-header" role="banner" data-module="govuk-header">
+		<div class="govuk-header__container app-width-container app-header__container">
+			<div class="govuk-header__logo app-header__logo">
+				<a class="govuk-header__link govuk-header__link--homepage" href="<?php echo esc_url( home_url() ); ?>">
 					<span class="govuk-header__logotype">
-						<img class="site-logo" src="<?php echo esc_url( $image[0] ); ?>" alt="" />
-					</span>
-				<?php endif; ?>
-				<span class="govuk-header__logotype-text">
 					<?php
-					if ( is_front_page() && is_home() ) :
+					$custom_logo_id = get_theme_mod( 'custom_logo' );
+					if ( $custom_logo_id ) :
+						$image = wp_get_attachment_image_src( $custom_logo_id, 'full' );
 						?>
-						<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
-						<?php
-					else :
-						?>
-						<p class="site-title"><?php bloginfo( 'name' ); ?></p>
-						<?php
-					endif;
-					?>
+						<img class="site-logo" src="<?php echo esc_url( $image[0] ); ?>" alt="" />
+					<?php endif; ?>
+						<span class="govuk-header__logotype-text">
+							<?php
+							if ( is_front_page() && is_home() ) :
+								?>
+								<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
+								<?php
+							else :
+								?>
+								<p class="site-title"><?php bloginfo( 'name' ); ?></p>
+								<?php
+							endif;
+							?>
+						</span>
+					</span>
+					<span class="govuk-header__product-name">
+						<?php bloginfo( 'description' ); ?>
+					</span>
 				</a>
-				</span>
-			</a>
 			</div>
-			<div class="govuk-grid-column-one-third">
-				<div class="bottom search-container">
-					<?php get_search_form(); ?>
-				</div>
+			<div class="app-site-search" data-module="app-search">
+				<?php get_search_form(); ?>
 			</div>
-		</div>
 		</div>
 	</header>
 
 
+	<nav class="app-navigation govuk-clearfix">
+		<ul class="app-navigation__list app-width-container">
+
+			<li class="app-navigation__list-item">
+				<a class="govuk-link govuk-link--no-visited-state govuk-link--no-underline app-navigation__link" href="/get-started/" data-topnav="Get started">Get started</a>
+			</li>
+
+			<li class="app-navigation__list-item">
+				<a class="govuk-link govuk-link--no-visited-state govuk-link--no-underline app-navigation__link" href="/styles/" data-topnav="Styles">Styles</a>
+			</li>
+
+			<li class="app-navigation__list-item">
+				<a class="govuk-link govuk-link--no-visited-state govuk-link--no-underline app-navigation__link" href="/components/" data-topnav="Components">Components</a>
+			</li>
+
+			<li class="app-navigation__list-item app-navigation__list-item--current">
+				<a class="govuk-link govuk-link--no-visited-state govuk-link--no-underline app-navigation__link" href="/patterns/" data-topnav="Patterns">Patterns</a>
+			</li>
+
+			<li class="app-navigation__list-item">
+				<a class="govuk-link govuk-link--no-visited-state govuk-link--no-underline app-navigation__link" href="/community/" data-topnav="Community">Community</a>
+			</li>
+
+		</ul>
+	</nav>
 
 	<div class="app-masthead">
 		<div class="app-width-container govuk-width-container">
@@ -80,23 +102,8 @@
 			</div>
 
 			<div class="govuk-grid-column-one-third-from-desktop">
-			<img class="app-masthead__image" src="/images/homepage-illustration.svg" alt="" role="presentation">
+			<img class="app-masthead__image" src="<?php bloginfo( 'template_url' ); ?>/images/homepage-illustration.svg" alt="" role="presentation">
 			</div>
 		</div>
-		</div>
-	</div>
-
-
-
-	<div class="govuk-width-container">
-		<div class="govuk-grid-row">
-			<div class="govuk-grid-column-two-thirds">
-				<p class="govuk-heading-xl"><?php bloginfo( 'description' ); ?></p>
-			</div>
-			<div class="govuk-grid-column-one-third">
-				<div class="bottom search-container">
-					<?php get_search_form(); ?>
-				</div>
-			</div>
 		</div>
 	</div>
--- searchform.php
+++ searchform.php
@@ -9,10 +9,9 @@
 
 ?>
 <div class="search" data-module="search">
-	<form action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get" role="search" class="search__form govuk-!-margin-bottom-4">
+	<form action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get" role="search">
 
 		<label class="govuk-label search__label" for="search" aria-hidden="true"><?php esc_html_e( 'Search this website', 'saltato' ); ?></label>
-		<input type="text" id="search" name="s" class="govuk-input govuk-!-margin-bottom-0 search__input" aria-controls="search-results" placeholder="<?php esc_html_e( 'Search', 'saltato' ); ?>" value="<?php echo get_search_query(); ?>">
-		<button type="submit" class="search__button" data-module="govuk-button"><?php esc_html_e( 'Search', 'saltato' ); ?></button>
+		<input type="text" id="search" name="s" class="govuk-input govuk-!-margin-bottom-0 search__input" aria-controls="search-results" placeholder="<?php esc_html_e( 'Search', 'saltato' ); ?> <?php bloginfo( 'name' ); ?>" value="<?php echo get_search_query(); ?>">
 	</form>
 </div>
